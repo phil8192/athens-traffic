b74d24566bdf2d4f50fa6b54a3a790969afa5b57
phil8192 <phil@parasec.net>
2014-11-11 22:18:24 +0200

    reversed latitude/longitude to longitude/latitude order

--- to-geojson.sh
+++ to-geojson.sh
@@ -4,10 +4,10 @@
 
 features() {
   local j=2
-  for i in $(cat data/linestrings.log) ;do
+  for i in $(cat data/linestrings.log |sed 's/,$//g; s/^\[//; s/\]$//g') ;do
     echo "{\"type\": \"Feature\", \"geometry\": {\"type\": \"LineString\", \"coordinates\": ["
-    echo $i |sed 's/,3/\n3/g' |sed 's/^/[/; s/$/],/' |tr -d "\n" |sed 's/,$//; s/^\[//'
-    echo "},"
+    echo $i |sed 's/,$//g; s/^\[//; s/\]$//g' |sed 's/,3/\n3/g' |awk -F ',' '{print $2 "," $1}' |sed 's/^/[/; s/$/],/' |tr -d "\n" |sed 's/,$//'
+    echo "]},"
     echo "\"properties\": {"
     tail -n+$j data/csv/road-segments.csv |head -1 |awk -F ',' '{print "\"id\": " $1 ", \"route\": \"" $2 "\", \"from\": \"" $3 "\", \"to\": \"" $4 "\", \"direction\": \"" $5 "\", \"length\": " $6}'
     echo "}},"
